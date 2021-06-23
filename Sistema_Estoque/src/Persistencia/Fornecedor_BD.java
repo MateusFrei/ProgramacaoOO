7c71659fc4cba83ce3f0b42b7403061a5a922e18
Mateus_Freitas <41398101+MateusFrei@users.noreply.github.com>
2021-06-23 00:33:39 -0300

    0

--- Sistema_Estoque/src/Persistencia/Fornecedor_BD.java
+++ Sistema_Estoque/src/Persistencia/Fornecedor_BD.java
@@ -1,5 +1,12 @@
 package Persistencia;
 
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+
+import java.sql.ResultSet;
+
+import Sistema_Controle_Estoque.Cliente;
+
 public class Fornecedor_BD {
 
 }
--- Sistema_Estoque/src/Persistencia/Funcionario_BD.java
+++ Sistema_Estoque/src/Persistencia/Funcionario_BD.java
@@ -1,5 +1,84 @@
 package Persistencia;
 
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+
+import java.sql.ResultSet;
+
+import Sistema_Controle_Estoque.Administrador;
+
 public class Funcionario_BD {
+	
+	public ArrayList<Administrador> listar_Funcionario()
+	{
+		try {
+			
+			Conexao_BD conecta = new Conexao_BD();
+			
+			String SQL = "SELECT * FROM administrador";
+			PreparedStatement stmt = conecta.conectar().prepareStatement(SQL);
+			
+			ResultSet rs = stmt.executeQuery();
+			
+			ArrayList<Administrador> lista_admin = new ArrayList<Administrador>();
+			
+			while(rs.next())
+			{
+				Administrador adm = new Administrador(
+						rs.getString("nome"),
+						rs.getString("cidade"),
+						rs.getString("email"),
+						rs.getString("tel"),
+						rs.getString("Cpf"),
+						rs.getString("login"),
+						rs.getString("senha"));
+				lista_admin.add(adm);
+			}
+			
+			stmt.close();
+			return lista_admin;
+			
+			
+		} catch (Exception ex) {
+			System.err.println("Erro geral"+ ex.getMessage());
+		}
+		return null;
+	}
+	
+	
+	public ArrayList<Cliente> Buscar_Cliente(String nome)
+	{
+		try {
+			
+			Conexao_BD conecta = new Conexao_BD();
+			
+			String SQL = "SELECT * FROM sistema_controle_estoque.cliente WHERE nome like '%"+nome+"%'";
+			PreparedStatement stmt = conecta.conectar().prepareStatement(SQL);
+			
+			ResultSet rs = stmt.executeQuery();
+			
+			ArrayList<Cliente> lista_cliente = new ArrayList<Cliente>();
+			
+			while(rs.next())
+			{
+				Cliente cli = new Cliente(
+						rs.getString("nome"),
+						rs.getString("cidade"),
+						rs.getString("email"),
+						rs.getString("tel"),
+						rs.getString("Cpf"));
+				lista_cliente.add(cli);
+			}
+			
+			stmt.close();
+			return lista_cliente;
+			
+			
+		} catch (Exception ex) {
+			System.err.println("Erro geral"+ ex.getMessage());
+		}
+		return null;
+	}
+	
 
 }
