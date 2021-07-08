f57b3784835e6f7f7fb5016d4e76e47c4001b29f
Mateus_Freitas <41398101+MateusFrei@users.noreply.github.com>
2021-07-08 17:58:11 -0300

    1,2

--- Sistema_Estoque/src/Apresentacao/Vendas.java
+++ Sistema_Estoque/src/Apresentacao/Vendas.java
@@ -1,15 +1,24 @@
 package Apresentacao;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import Persistencia.Conexao_BD;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.awt.event.ActionEvent;
 
+@SuppressWarnings("serial")
 public class Vendas extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -36,8 +45,61 @@ public class Vendas extends JFrame {
 		setBounds(100, 100, 800, 503);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(344, 74, 187, 162);
+		contentPane.add(textArea);
+		
+		JLabel lblNewLabel = new JLabel("Quantidade:");
+		lblNewLabel.setBounds(40, 44, 116, 23);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Valor Total:");
+		lblNewLabel_1.setBounds(40, 84, 80, 29);
+		contentPane.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(99, 46, 116, 19);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(96, 89, 119, 19);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Finalizar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				try 
+				{
+					Conexao_BD con = new Conexao_BD();
+					
+					String query = "INSERT INTO vendas (quantidade, valor_total) VALUES (?, ?)";
+					
+					PreparedStatement stmt = con.conectar().prepareStatement(query);
+					
+					stmt.setString(1, textField.getText());
+					stmt.setString(2, textField_1.getText());
+					stmt.executeUpdate();
+					stmt.close();
+					
+					System.out.println("Venda submetida.");
+					System.out.println("Conection closed...");
+					
+				}catch (Exception x) {
+					System.err.println("Erro ao conectar !" + x.getMessage());
+				}
+			}
+		});
+		btnNewButton.setBounds(107, 145, 85, 21);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setBounds(344, 51, 45, 13);
+		contentPane.add(lblNewLabel_2);
 	}
-
 }
