972b22640cf2be9035b5f789a2568170b5bbc5e9
Mateus_Freitas <41398101+MateusFrei@users.noreply.github.com>
2021-07-07 21:07:56 -0300

    1,2

--- Sistema_Estoque/src/Apresentacao/Estoque.java
+++ Sistema_Estoque/src/Apresentacao/Estoque.java
@@ -32,7 +32,7 @@ public class Estoque extends JFrame {
 	 */
 	public Estoque() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 801, 498);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
